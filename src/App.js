75439b65034db2ede501e51baa990e0c652e6971
vsparrow.com <khan@vsparrow.com>
2020-01-20 22:43:49 +0000

    add inital filter selections: '

--- src/App.js
+++ src/App.js
@@ -2,13 +2,22 @@ import React	 from 'react'
 import NewNote from './components/NewNote'
 import Note from './components/Note'
 
-const App = ({store})=>{	
+const App = ({store})=>{
+	const radioSelection = {type: 'radio', name: 'filter'}
+	const filterSelected = value => () => {console.log(value)}
 	return(
 		<div>
 			<NewNote store={store}/>
+			<div>
+				all <input type='radio' name='filter' onChange={filterSelected('ALL')}/>
+				important <input {...radioSelection} onChange={filterSelected('IMPORTANT')}/>
+				nonimportant <input {...radioSelection} onChange={filterSelected('NONIMPORTANT')}/>
+			</div>
 			<Note store={store} />
 		</div>
 	)	
 }
+// important   <input type="radio" name="filter" onChange={filterSelected('IMPORTANT')} />          nonimportant <input type="radio" name="filter" onChange={filterSelected('NONIMPORTANT')} />
+
 
 export default App
