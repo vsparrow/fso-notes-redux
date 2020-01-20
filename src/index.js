87002f0f6096c3ffd3a159dcab0fdab26208940c
vsparrow.com <khan@vsparrow.com>
2020-01-20 22:32:13 +0000

    set initial set via reducer rather than dispatch from index

--- src/index.js
+++ src/index.js
@@ -6,10 +6,11 @@ import noteReducer from './reducers/noteReducer'
 
 const store = createStore(noteReducer)
 
-const note1 = {    content: 'the app state is in redux store',    important: true,    id: 1  }
-const note2 = {    content: 'state changes are made with actions',    important: false,    id: 2  }
-store.dispatch({type: 'NEW_NOTE', data: note1})
-store.dispatch({type: 'NEW_NOTE', data: note2})
+//moved to notesReducer as initalState
+// const note1 = {    content: 'the app state is in redux store',    important: true,    id: 1  }
+// const note2 = {    content: 'state changes are made with actions',    important: false,    id: 2  }
+// store.dispatch({type: 'NEW_NOTE', data: note1})
+// store.dispatch({type: 'NEW_NOTE', data: note2})
 		
 
 const renderApp = ()=>ReactDOM.render(<App store={store}/>,document.getElementById('root'))
