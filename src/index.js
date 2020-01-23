484bba71f848bf6a0f479b98614def4941eb6937
vsparrow.com <khan@vsparrow.com>
2020-01-23 20:08:16 +0000

    update baseUrl

--- src/index.js
+++ src/index.js
@@ -1,18 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// import {createStore, combineReducers} from 'redux'
 import {Provider} from 'react-redux'
 import App from './App';
-// import noteReducer from './reducers/noteReducer'
-// import filterReducer from './reducers/filterReducer'
 import store from './store'
-// const reducer = combineReducers({notes: noteReducer, filter: filterReducer})
-// const store = createStore(reducer)
 
 console.log(store.getState())
 
 ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'))
-// const renderApp = ()=>ReactDOM.render(<App store={store}/>,document.getElementById('root'))
-// renderApp()
-// store.subscribe(renderApp)
 
--- src/services/notes.js
+++ src/services/notes.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const baseUrl = 'http://localhost:3001/notes'
+const baseUrl = 'http://korea-ide-api-3001.run.goorm.io/notes'
 
 const getAll = async ()=>{
 	const respose = await axios.get(baseUrl)
