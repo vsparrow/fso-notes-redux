38fda0c2a32c01aebccc30ca0627450b9e5d03b1
vsparrow.com <khan@vsparrow.com>
2020-01-24 00:27:21 +0000

    getting notes from backend

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
-import React	 from 'react'
+import React,{useEffect} from 'react'
+import {connect} from 'react-redux'
 import NewNote from './components/NewNote'
 import Notes from './components/Notes'
 import VisibilityFilter from './components/VisibilityFilter'
+import {initializeNotes} from './reducers/noteReducer'
 
-const App = ({store})=>{
-
+const App = (props)=>{
+	useEffect(()=>{props.initializeNotes()},[])
 	return(
 		<div>
-			<NewNote store={store}/>
-			<VisibilityFilter store={store} />
-			<Notes store={store} />
+			<NewNote />
+			<VisibilityFilter  />
+			<Notes  />
 		</div>
 	)	
 }
 
-export default App
+export default connect(null,{initializeNotes})(App)
