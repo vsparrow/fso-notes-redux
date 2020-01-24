683b506276be79cc0a7879550bb38f2d7b5a18cc
vsparrow.com <khan@vsparrow.com>
2020-01-24 00:58:46 +0000

    make sure creating notes hits backend

--- src/reducers/noteReducer.js
+++ src/reducers/noteReducer.js
@@ -1,23 +1,8 @@
 import noteService from '../services/notes'
-//we will get state from backend
-// const initialState = [
-//   {
-//     content: 'reducer defines how redux store works',
-//     important: true,
-//     id: 1,
-//   },
-//   {
-//     content: 'state of store can contain any data',
-//     important: false,
-//     id: 2,
-//   },
-// ]
-
 
 const noteReducer = (state=[], action)=>{
 	switch(action.type){
 		case 'NEW_NOTE':
-			// return state.concat(action.data)
 			return [...state, action.data]
 		case 'INIT_NOTES': //get remote data and set to state
 			return action.data
@@ -31,16 +16,19 @@ const noteReducer = (state=[], action)=>{
 	}
 }
 
-//helper
-// const generateId = ()=>Number((Math.random()* 1000000).toFixed(0))
-
 //action creators
-export const createNote = content => ({type: 'NEW_NOTE', data: {content, important: false, }})
 export const toggleImportanceOf = id =>({type: 'TOGGLE_IMPORTANCE', data: {id}})
 export const initializeNotes = ()=>{ 
 	return async dispatch => {
 		const notes = await noteService.getAll()
 		dispatch({type: 'INIT_NOTES',data: notes})
+	}
+}
+// export const createNote = content => ({type: 'NEW_NOTE', data: {content, important: false, }}) //old pre-backend
+export const createNote = content => {
+	return async dispatch =>{
+		const newNote = await noteService.createNew(content)
+		dispatch({type: 'NEW_NOTE', data:newNote})
 	}
 }
 
